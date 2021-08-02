4ff88dada625938e6c572f619d3b3fedfd1ddb72
mtp.blog.site <mtp.blog.site@gmail.com>
2021-08-02 14:06:35 +0800

    computation almost good

--- battlestat-backup8-2-2021.js
+++ battlestat-backup8-2-2021.js
@@ -2,13 +2,13 @@
 let weaponExtraPowerSet = {
 	charAndWeaponSame: .002675,
 	weaponAndPowerSame: .002575,
-	default: .0025
+	dafault: .0025
 };
 let traitBonusSet = {
 	charAndWeaponTrait: 0.075,
 	greaterThanEnemy: 0.075,
-	lessThanEnemy: 0.075,
-	default: 0
+	lessThanEnemy: - 0.075,
+	dafault: 0
 };
 let traitStrengthSet = {
 	earth: 'lightning',
@@ -32,8 +32,7 @@ let weaponTraitIconMap = {
 // interface / Model
 let characterStatsModel = {
 	trait: '',
-	power: 0,
-	level: 0
+	power: 0
 };
 let weaponStatsModel = {
 	id: 0,
@@ -70,17 +69,11 @@ function getTrait(traitIcon, map) {
 	return map[traitIcon];
 }
 
-function getCharacterPower(level) {
-    return ((1000 + (level * 10)) * (Math.floor(level / 10) + 1));
-}
-
 function getCharacter() {
 	// Get Nature
 	characterStatsModel['trait'] = getTrait(document.getElementsByClassName("trait-icon")[0].classList[0], traitIconMap);
 	// Get Power
 	characterStatsModel['power'] = parseFloat(document.getElementsByClassName("subtext-stats")[0].childNodes[5].textContent.replace(/,/g, ''));
-	// Get Level
-	characterStatsModel['level'] = parseFloat(document.getElementsByClassName("subtext-stats")[0].childNodes[2].textContent.replace(/\(.*?\)/gi, ''));
 
 	return characterStatsModel;
 }
@@ -136,87 +129,99 @@ function getEnimies() {
 }
 
 function getTraitBonus(character, enemy, weapon) {
-	traitBonus = 1;
+	traitBonus = 0;
 	if (character.trait == weapon.trait) {
-		traitBonus = traitBonus + (1 * traitBonusSet.charAndWeaponTrait);
+		traitBonus = traitBonusSet.charAndWeaponTrait;
 	} 
 	
 	// Check weapon bonus base on enemy's traits
 	if (compareTrait(character.trait, enemy.trait) == 'greater') {
-		traitBonus = traitBonus + (1 * traitBonusSet.greaterThanEnemy);
+		traitBonus = traitBonusSet.greaterThanEnemy;
 	} else if (compareTrait(character.trait, enemy.trait) == 'lesser') {
-		traitBonus = traitBonus + (-1 * traitBonusSet.lessThanEnemy);
+		traitBonus = traitBonusSet.lessThanEnemy;
 	} else {
-		traitBonus = traitBonus + traitBonusSet.default;
+		traitBonus = traitBonusSet.dafault;
 	}
 
 	return traitBonus;
 }
 
 function getWeaponPower(character, weapon) {
-	weaponPower = 1;
+	weaponPower = 0;
 	for (i=0; i< _.size(weapon.stats); i++) {
 		// Add on per weapon stats
 		if (character.trait == weapon.trait) {
-			//weaponPower = weaponPower + (weapon.stats[i].power * weaponExtraPowerSet.charAndWeaponSame)
+			weaponPower = weaponPower + (weapon.stats[i].power * weaponExtraPowerSet.charAndWeaponSame)
 		} 
 		
 		if (weapon.trait == weapon.stats[i].trait) {
 			// TODO: Review this line if its - if-if or if-elseif
 			weaponPower = weaponPower + (weapon.stats[i].power * weaponExtraPowerSet.weaponAndPowerSame)
 		}else {
-			weaponPower = weaponPower + (weapon.stats[i].power * weaponExtraPowerSet.default)
+			weaponPower = weaponPower + (weapon.stats[i].power * weaponExtraPowerSet.dafault)
 		}
 	}
 
-	return weaponPower;
+	return (weaponPower + 1);
 }
 
-function getCharacterTotalPower(characterPower, character, weapon, weaponPower, traitBonus) {
+function getCharacterTotalPower(characterPower, weapon, weaponPower, traitBonus) {
 	// Weapon bonus
 	weaponBonus = 0;
 	if (_.size(weapon.bonus) > 1) {
 		weaponBonus = parseInt(weapon.bonus[0].bonus);
 	}
 	// Base Character power * weapon power X weaponBonus
-	heroPowerWithWeapon = (getCharacterPower(character.level) * weaponPower) + weaponBonus;
+	heroPowerWithWeapon = (characterPower * weaponPower)+weaponBonus;
 	// 10 percent chance to increse
-	heroPowerWithPlus10Percent = heroPowerWithWeapon * traitBonus * 1.1;
+	heroPowerWithPlus10Percent = heroPowerWithWeapon + (heroPowerWithWeapon * .1);
 	// 10 percent chance to decrease
-	heroPowerWithMinus10Percent = heroPowerWithWeapon * traitBonus * .9;
-	characterRange = heroPowerWithPlus10Percent - heroPowerWithMinus10Percent;
+	heroPowerWithMinus10Percent = heroPowerWithWeapon - (heroPowerWithWeapon * .1);
+	// Add weaponBonus
+	heroPowerWithPlus10Percent = heroPowerWithPlus10Percent * (1 + traitBonus);
+	heroPowerWithMinus10Percent = heroPowerWithMinus10Percent * (1 + traitBonus);
 	// Create an object for function return
-	characterPowerRange = {plus: heroPowerWithPlus10Percent, minus: heroPowerWithMinus10Percent, range: characterRange};
+	characterPowerRange = {plus: heroPowerWithPlus10Percent, minus: heroPowerWithMinus10Percent};
 
 	return characterPowerRange;
 }
 
 function getEnemyTotalPower(enemyPower) {
 	// Compute for +/- 10%
-	enemyWithPlus10Percent = enemyPower.power * 1.1;
-	enemyWithMinus10Percent = enemyPower.power * 0.9;
-	enemyRange = enemyWithPlus10Percent - enemyWithMinus10Percent;
+	enemyWithPlus10Percent = enemyPower.power + (enemyPower.power * .1);
+	enemyWithMinus10Percent = enemyPower.power - (enemyPower.power * .1);
 
 	// Range of probability
-	enemyPowerRange = {plus: enemyWithPlus10Percent, minus: enemyWithMinus10Percent, range: enemyRange};
+	enemyPowerRange = {plus: enemyWithPlus10Percent, minus: enemyWithMinus10Percent};
 
 	return enemyPowerRange;
 }
 
 function battle(characterPower, enemyPower) {
-	let win = 0;
-    let lose = 0;
-    for (let playerRoll = Math.floor(characterPower.minus); playerRoll <= characterPower.plus; playerRoll++) {
-      for (let enemyRoll = Math.floor(enemyPower.minus); enemyRoll <= enemyPower.plus; enemyRoll++) {
-        if (playerRoll >= enemyRoll) {
-          win++;
-        } else {
-          lose++;
-        }
-      }
-    }
+	// Get range from worst to best probability
+	percentage = 0;
+	
+	// Worst probability - enemy has +10% while you have -10%
+	console.log('Worst Case: You' + characterPower.minus + ' vs Enemy' + enemyPower.plus);
+	worstCase = {enemy: enemyPower.minus, character: characterPower.plus};
+	worstCase = characterPower.minus - enemyPower.plus;
+	console.log(worstCase);
+	if (worstCase > 0) {
+		// 100% of winning
+		percentage = 100;
+	} else {
+		percentage = (worstCase / characterPower.minus);
+	}
+	console.log(percentage);
 
-    return win / (win + lose);
+	// Best probability - enemy has +10% while you have -10%
+	bestCase = {enemy: enemyPower.plus, character: characterPower.minus}
+	console.log('Best Case: ' + characterPower.plus + ' vs ' + enemyPower.minus);
+
+	return {
+		bestCase: bestCase,
+		worstCase: worstCase
+	};
 }
 
 function addDiv(count, stats) {
@@ -250,9 +255,8 @@ for (x=0; x<enemies.length; x++) {
 	console.log('Enemy Number: ' + (x+1));
 	weaponPower = getWeaponPower(character, weapon);
 	traitBonus = getTraitBonus(character, enemies[x], weapon);
-	characterTotalPower = getCharacterTotalPower(character.power, character, weapon, weaponPower, traitBonus);
+	characterTotalPower = getCharacterTotalPower(character.power, weapon, weaponPower, traitBonus);
 	enemyTotalPower = getEnemyTotalPower(enemies[x]);
 	battleProbability = battle(characterTotalPower, enemyTotalPower, x);
-	console.log(battleProbability);
 	addDiv(x, battleProbability);
 }
